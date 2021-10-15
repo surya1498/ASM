1279545879e45c1dbe1a570f882f00e9c6fe74c7
surya1498 <69519458+surya1498@users.noreply.github.com>
2021-10-15 08:19:13 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,8 @@
 const path = require("path");
 
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const fs = require('fs');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
 const mnemonic = fs.readFileSync(".secret").toString().trim();
-
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -11,49 +10,68 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     development: {
-      host: 'localhost',
-      network_id: '*',
-      port: 8545,
-     
+      host: "localhost",
+      network_id: "*",
+      port: 9545,
     },
     bsctestnet: {
-      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://data-seed-prebsc-1-s1.binance.org:8545`
+        ),
       network_id: 97,
       confirmations: 10,
       timeoutBlocks: 200,
       skipDryRun: true,
     },
     bscmainnet: {
-      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      provider: () =>
+        new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
       network_id: 56,
       confirmations: 10,
       timeoutBlocks: 200,
-      skipDryRun: true
+      skipDryRun: true,
     },
     matictestnet: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+      provider: () =>
+        new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
       network_id: 80001,
       confirmations: 2,
       timeoutBlocks: 300,
-      skipDryRun: true
+      skipDryRun: true,
     },
     ropsten: {
-      provider: function() { 
-       return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/e94f63aab58d4c4780ad1b274243fded");
+      provider: function () {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/v3/7477dde235a14bb389654fb37ff948d6"
+        );
       },
       network_id: 3,
-   
-  }
+      confirmations: 2,
+      timeoutBlocks: 300,
+      skipDryRun: true,
+      
+    
+    },
   },
   compilers: {
     solc: {
-      version: "^0.8.0",
-      settings: { // See the solidity docs for advice about optimization and evmVersion
+      version: "^0.8.7",
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
-            enabled: true,
-            runs: 200
-        }
-    }
-    }
+          enabled: false,
+          runs: 200,
+        },
+      },
+    },
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: '3WFYZTDS9XY5BQIXDFK7XND28XD9Q6EN7S'
   }
 };
